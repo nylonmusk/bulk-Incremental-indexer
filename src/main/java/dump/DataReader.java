998660257aca732d53efe0fb96b2a47368082bc7
nylonmusk <2912rla@gmail.com>
2024-03-26 18:03:29 +0900

    refactor : update시 필요한 readJsonFileToString 메소드 생성

--- src/main/java/dump/DataReader.java
+++ src/main/java/dump/DataReader.java
@@ -5,12 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class DataReader {
-    public List<Map<String, Object>> readJsonFile(String filePath) throws IOException {
+    public List<Map<String, Object>> readJsonFileToList(String filePath) throws IOException {
         String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
-        return new ObjectMapper().readValue(jsonContent, List.class);
+        return Collections.unmodifiableList(new ObjectMapper().readValue(jsonContent, List.class));
+    }
+
+    public String readJsonFileToString(String filePath) throws IOException {
+        String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
+        return jsonContent;
     }
 }

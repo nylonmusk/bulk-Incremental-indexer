3ac45f053ce450b0db86777491eea80cd784d6e0
nylonmusk <2912rla@gmail.com>
2024-03-26 18:04:42 +0900

    refactor : 불필요한 isValid 메소드 삭제

--- src/main/java/validation/ConfigValidator.java
+++ src/main/java/validation/ConfigValidator.java
@@ -4,16 +4,11 @@ import java.util.Map;
 
 public class ConfigValidator {
 
-
     public static boolean isValid(Map<String, Map<String, Object>> configData, String type) {
         return configData.get(type) != null && !configData.get(type).isEmpty();
     }
 
     public static boolean isValid(Map<String, Map<String, Object>> configData, String type, String keyword) {
         return configData.get(type).get(keyword) != null && !configData.get(type).get(keyword).toString().isEmpty();
-    }
-
-    public static boolean isValid(String type, Map<String, Object> specificConfigData) {
-        return specificConfigData.get(type) != null && !specificConfigData.get(type).toString().isEmpty();
     }
 }
